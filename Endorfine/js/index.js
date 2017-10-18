fbf0e657cfc45dd3d896eabaa19dcf785e22d41d
Vadim <vdk121213@gmail.com>
2017-10-18 12:11:32 +0300

    Endorfine_V0.6
    Item-image update

--- Endorfine/js/index.js
+++ Endorfine/js/index.js
@@ -4,38 +4,30 @@ $(document).ready(function(){
 	$('.visual-content').click(function(){
 		$('body').removeClass('bg-non')
 		$('.gallery').removeClass('text-page')
-		$('#usun-tatuaz').addClass('non')
-		$('#regulamin').addClass('non')
-		$('#studio').removeClass('non')
-		$('#galeria').removeClass('non');
-		$('.hidden-content').removeClass('non');
+		$('#studio, #galeria, .hidden-content').removeClass('non')
+		$('#usun-tatuaz, #regulamin').addClass('non')
 	})
 
 	/*USUŃ TATUAŻ*/
 	$('#text-content-1').click(function(){
+		$('#usun-tatuaz').removeClass('non')
 		$('body').addClass('bg-non')
 		$('.gallery').addClass('text-page')
-		$('#regulamin').addClass('non')
-		$('#usun-tatuaz').removeClass('non')
-		$('#studio').addClass('non')
-		$('#galeria').addClass('non')
-		$('.hidden-content').addClass('non');
+		$('#studio, #galeria, #regulamin, .hidden-content').addClass('non')
 	})
 
 	//REGULAMIN
 	$('#text-content-2').click(function(){
+		$('#regulamin').removeClass('non')
 		$('body').addClass('bg-non')
 		$('.gallery').addClass('text-page')
-		$('#usun-tatuaz').addClass('non')
-		$('#regulamin').removeClass('non')
-		$('#studio').addClass('non')
-		$('#galeria').addClass('non')
-		$('.hidden-content').addClass('non');
+		$('#studio, #galeria, #usun-tatuaz, .hidden-content').addClass('non')
 	})
 	//MENU
 	$('#menu-button').click(function(){
 		$('.navigation').slideToggle(100);
 	});
+
 	$(window).resize(function(){
 		if($(window).width() > 425 ){			
 			$('.navigation').removeAttr('style');
@@ -46,100 +38,100 @@ $(document).ready(function(){
 	$('#hex-1')
 	.click(function(){
 		$(this).toggleClass('active-hex-1-2')
-		$('#slider-1').slideToggle()
 		$('#hex-2, #hex-3, #hex-4, #hex-5, #hex-6, #hex-7, #hex-8, #hex-9, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-1-3');
+		$('.hidden-content').toggleClass('up-1-3')
+		$('#slider-1').slideToggle();
 	})
 
 	//SLIDER-2
 	$('#hex-2').click(function(){
 		$(this).toggleClass('active-hex-1-2')
-		$('#slider-2').slideToggle()
 		$('#hex-1, #hex-3, #hex-4, #hex-5, #hex-6, #hex-7, #hex-8, #hex-9, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-1-3');
+		$('.hidden-content').toggleClass('up-1-3')
+		$('#slider-2').slideToggle();
 	})
 
 	//SLIDER-3
 	$('#hex-3').click(function(){
 		$(this).toggleClass('active-hex-3')
-		$('#slider-3').slideToggle()
 		$('#hex-1, #hex-2, #hex-4, #hex-5, #hex-6, #hex-7, #hex-8, #hex-9, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-1-3');
+		$('.hidden-content').toggleClass('up-1-3')
+		$('#slider-3').slideToggle();
 	})
 
 	//SLIDER-4
 	$('#hex-4').click(function(){
 		$(this).toggleClass('active-hex-4-5')
-		$('#slider-4').slideToggle()
 		$('#hex-1, #hex-2, #hex-3, #hex-5, #hex-6, #hex-7, #hex-8, #hex-9, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-4-8');
+		$('.hidden-content').toggleClass('up-4-8')
+		$('#slider-4').slideToggle();
 	})
 
 	//SLIDER-5
 	$('#hex-5').click(function(){
 		$(this).toggleClass('active-hex-4-5')
-		$('#slider-5').slideToggle()
 		$('#hex-1, #hex-2, #hex-3, #hex-4, #hex-6, #hex-7, #hex-8, #hex-9, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-4-8');
+		$('.hidden-content').toggleClass('up-4-8')
+		$('#slider-5').slideToggle();
 	})
 
 	//SLIDER-6
 	$('#hex-6').click(function(){
 		$(this).toggleClass('active-hex-6-8')
-		$('#slider-6').slideToggle()
 		$('#hex-1, #hex-2, #hex-3, #hex-4, #hex-5, #hex-7, #hex-8, #hex-9, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-4-8');
+		$('.hidden-content').toggleClass('up-4-8')
+		$('#slider-6').slideToggle();
 	})
 
 	//SLIDER-7
 	$('#hex-7').click(function(){
 		$(this).toggleClass('active-hex-6-8')
-		$('#slider-7').slideToggle()
 		$('#hex-1, #hex-2, #hex-3, #hex-4, #hex-5, #hex-6, #hex-8, #hex-9, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-4-8');
+		$('.hidden-content').toggleClass('up-4-8')
+		$('#slider-7').slideToggle();
 	})
 
 	//SLIDER-8
 	$('#hex-8').click(function(){
 		$(this).toggleClass('active-hex-6-8')
-		$('#slider-8').slideToggle()
 		$('#hex-1, #hex-2, #hex-3, #hex-4, #hex-5, #hex-6, #hex-7, #hex-9, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-4-8');
+		$('.hidden-content').toggleClass('up-4-8')
+		$('#slider-8').slideToggle();
 	})
 
 	//SLIDER-9
 	$('#hex-9').click(function(){
 		$(this).toggleClass('active-hex-9-11')
-		$('#slider-9').slideToggle()
 		$('#hex-1, #hex-2, #hex-3, #hex-4, #hex-5, #hex-6, #hex-7, #hex-8, #hex-10, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-9-11');
+		$('.hidden-content').toggleClass('up-9-11')
+		$('#slider-9').slideToggle();
 	})
 
 	//SLIDER-10
 	$('#hex-10').click(function(){
 		$(this).toggleClass('active-hex-9-11')
-		$('#slider-10').slideToggle()
 		$('#hex-1, #hex-2, #hex-3, #hex-4, #hex-5, #hex-6, #hex-7, #hex-8, #hex-9, #hex-11, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-9-11');
+		$('.hidden-content').toggleClass('up-9-11')
+		$('#slider-10').slideToggle();
 	})
 
 	//SLIDER-11
 	$('#hex-11').click(function(){
 		$(this).toggleClass('active-hex-9-11')
-		$('#slider-11').slideToggle()
 		$('#hex-1, #hex-2, #hex-3, #hex-4, #hex-5, #hex-6, #hex-7, #hex-8, #hex-9, #hex-10, .zest').toggleClass('non')
 		$('.hex-wrap').toggleClass('hex-wrap-active')
-		$('.hidden-content').toggleClass('up-9-11');
+		$('.hidden-content').toggleClass('up-9-11')
+		$('#slider-11').slideToggle();
 	})
 
 });
